1677c4d7b80e281c34ec492537f1cc15d6e62aa8
Colin Nguyen <nguyen_c9@denison.edu>
2026-07-09 13:46:52 -0400

    Move file to src folder

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    
+    // default port
+    int port = 6379;
+    if (argc >= 2) {
+        port = std::stoi(argv[1]);
+    }
+    
+    return 0;
+}
